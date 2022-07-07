b75cd35a87abedee3e6baa7bcc6185a09f8ac14a
k.stoetter <katrin.stoetter@stud-mail.uni-wuerzburg.de>
2022-07-07 12:23:08 +0200

    hübsche Farben, TODO: Rastervisitor anschauen, ovales objekt ist immernoch schwart

--- src/raster-box.ts
+++ src/raster-box.ts
@@ -67,6 +67,13 @@ export default class RasterBox {
         ];
         let color = [
             1,0,0,1,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0.5, 0, 0, 1,
+            1, 1, 0, 1,
+            1, 0, 1, 1,
+            0, 1, 1, 1,
+            0, 0.5, 1, 1
         ]
         const vertexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
