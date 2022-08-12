bf1c96f831cd1d3a1549e0681153dc8dd3486d3d
s416007 <IchMAchs1009+?>
2022-08-12 12:16:27 +0200

    irgendwas geht aber komisch rotated in raytracer

--- src/index.ts
+++ src/index.ts
@@ -13,20 +13,23 @@ import {
 import Shader from './shader';
 import perspectiveVertexShader from './perspective-vertex-shader.glsl';
 import fragmentShader from './basic-fragment-shader.glsl'
-import { Scaling, Translation } from './transformation';
+import {Rotation, Scaling, Translation} from './transformation';
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById("rasteriser") as HTMLCanvasElement;
     const gl = canvas.getContext("webgl2");
 
     // construct scene graph
-    const sg = new GroupNode(new Scaling(new Vector(1, 0.2, 0.1, 0)));
-    const gn1 = new GroupNode(new Translation(new Vector(1, 1, 1, 0)));
-    sg.add(gn1);
-    let gn2 = new GroupNode(new Translation(new Vector(0, 0, 0, 0)));
-    sg.add(gn2);
-    const cube = new AABoxNode(new Vector(0, 1, 1, 1));
-    gn2.add(cube);
+    const sg = new GroupNode(new Translation(new Vector(0, 0, 0, 0)));
+    const gnRotation = new Rotation(new Vector(-1, 1, 0, 0), 0)
+    const gn = new GroupNode(gnRotation);
+    sg.add(gn);
+    // const gn = new GroupNode(new Translation(new Vector(1, 1, 1, 0)));
+    // sg.add(gn);
+    // let gn2 = new GroupNode(new Translation(new Vector(0, 0, 0, 0)));
+    // sg.add(gn2);
+    const cube = new AABoxNode(new Vector(0, 0, 0, 0));
+    gn.add(cube);
 
     // setup for rendering
     const setupVisitor = new RasterSetupVisitor(gl);
@@ -47,17 +50,19 @@ window.addEventListener('load', () => {
     );
     const visitor = new RasterVisitor(gl, shader, null, setupVisitor.objects);
 
-    function animate(timestamp: number) {
-        camera.eye = new Vector(
-            Math.cos(timestamp / 1000),
-            0,
-            Math.sin(timestamp / 1000),
-            1
-        );
-        visitor.render(sg, camera, []);
-        window.requestAnimationFrame(animate);
-    }
+    // function animate(timestamp: number) {
+    //     camera.eye = new Vector(
+    //         Math.cos(timestamp / 1000),
+    //         0,
+    //         Math.sin(timestamp / 1000),
+    //         1
+    //     );
+    //     visitor.render(sg, camera, []);
+    //     window.requestAnimationFrame(animate);
+    // }
+    //
+    shader.load();
+    visitor.render(sg, camera, []);
 
-    shader.load()
-    window.requestAnimationFrame(animate);
+    // window.requestAnimationFrame(animate);
 });
