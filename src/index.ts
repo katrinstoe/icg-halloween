cc26ab5f90d4e007241b9b22fab4dbdde6d517af
Lina Seyfried <lina.seyfried@stud-mail.uni-wuerzburg.de>
2022-08-12 12:05:20 +0200

    taskleiste aber sie dreht sich

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,63 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
-import Sphere from "./sphere";
-import Vector from "./vector";
-import Ray from "./ray";
-import Intersection from "./intersection";
-import phong from "./phong";
-import RayVisitor from './rayvisitor';
-import {GroupNode, SphereNode} from "./nodes";
-import {Rotation, Scaling, Translation} from "./transformation";
-
-let theCanvas: HTMLCanvasElement;
-
-function reply_click(clicked_id: any)
-{
-    alert(clicked_id);
-    if (clicked_id == "rayTracerBtn"){
-        theCanvas = document.getElementById("raytracer") as HTMLCanvasElement;
-    }
-    else {
-        theCanvas = document.getElementById("rasterizer") as HTMLCanvasElement;
-    }
-}
+import Vector from './vector';
+import {
+    GroupNode,
+    SphereNode,
+    AABoxNode
+} from './nodes';
+import {
+    RasterVisitor,
+    RasterSetupVisitor
+} from './rastervisitor';
+import Shader from './shader';
+import perspectiveVertexShader from './perspective-vertex-shader.glsl';
+import fragmentShader from './basic-fragment-shader.glsl'
+import { Scaling, Translation } from './transformation';
 
 window.addEventListener('load', () => {
-    const canvas = document.getElementById("raytracer") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("rasteriser") as HTMLCanvasElement;
+    const gl = canvas.getContext("webgl2");
 
-    //create
-    const sg = new GroupNode(new Translation(new Vector(0, 0, -5, 0)));
-    const gnRotation = new Rotation(new Vector(1, 0, 0, 0), 0)
-    const gn = new GroupNode(gnRotation);
-    sg.add(gn);
+    // construct scene graph
+    const sg = new GroupNode(new Scaling(new Vector(1, 0.2, 0.1, 0)));
+    const gn1 = new GroupNode(new Translation(new Vector(1, 1, 1, 0)));
+    sg.add(gn1);
+    let gn2 = new GroupNode(new Translation(new Vector(0, 0, 0, 0)));
+    sg.add(gn2);
+    const cube = new AABoxNode(new Vector(0, 1, 1, 1));
+    gn2.add(cube);
 
-    const gnS = new GroupNode(new Translation(new Vector(1, 0.5, 0, 0)));
-    gn.add(gnS);
-    gnS.add(new SphereNode(new Vector(1, 0.9, 0.1, 1)));
-
-    const gnE = new GroupNode(new Translation(new Vector(5,-0.8,0,0)));
-    gnS.add(gnE);
-
-    const gnEs = new GroupNode(new Scaling(new Vector(0.35,0.35,0.35,0.35)));
-    gnE.add(gnEs)
-
-    gnEs.add(new SphereNode(new Vector(0.05, 0.35, 0.2, 1)));
-
-
-    /*const gnM = new GroupNode(new Scaling(new Vector(0.3, 0.3, 0.3, 0.3)));
-    const gnMt = new GroupNode(new Translation(new Vector(6,0,0,0)));
-    gnM.add(gnMt);
-    gnMt.add(new SphereNode(new Vector(0.2, 0.15, 0.2, 1)));
-    gnEt.add(gnM);*/
-
-
-    const lightPositions = [
-        new Vector(1, 1, -1, 1)
-    ];
+    // setup for rendering
+    const setupVisitor = new RasterSetupVisitor(gl);
+    setupVisitor.setup(sg);
 
     const camera = {
-        origin: new Vector(0, 0, 0, 1),
-        width: canvas.width,
-        height: canvas.height,
-        alpha: Math.PI / 3
-    }
+        eye: new Vector(-.5, .5, -1, 1),
+        center: new Vector(0, 0, 0, 1),
+        up: new Vector(0, 1, 0, 0),
+        fovy: 60,
+        aspect: canvas.width / canvas.height,
+        near: 0.1,
+        far: 100
+    };
+    const shader = new Shader(gl,
+        perspectiveVertexShader,
+        fragmentShader
+    );
+    const visitor = new RasterVisitor(gl, shader, null, setupVisitor.objects);
 
-    const visitor = new RayVisitor(ctx, canvas.width, canvas.height);
-
-    let animationHandle: number;
-
-    let lastTimestamp = 0;
-    let animationTime = 0;
-    let animationHasStarted = true;
     function animate(timestamp: number) {
-        let deltaT = timestamp - lastTimestamp;
-        if (animationHasStarted) {
-            deltaT = 0;
-            animationHasStarted = false;
-        }
-        animationTime += deltaT;
-        lastTimestamp = timestamp;
-        gnRotation.angle = animationTime / 2000;
-
-        visitor.render(sg, camera, lightPositions);
-        // animationHandle = window.requestAnimationFrame(animate);
+        camera.eye = new Vector(
+            Math.cos(timestamp / 1000),
+            0,
+            Math.sin(timestamp / 1000),
+            1
+        );
+        visitor.render(sg, camera, []);
+        window.requestAnimationFrame(animate);
     }
 
-    function startAnimation() {
-        if (animationHandle) {
-            window.cancelAnimationFrame(animationHandle);
-        }
-        animationHasStarted = true;
-        function animation(t: number) {
-            animate(t);
-            animationHandle = window.requestAnimationFrame(animation);
-        }
-        animationHandle = window.requestAnimationFrame(animation);
-    }
-    animate(0);
-
-    document.getElementById("startAnimationBtn").addEventListener(
-        "click", startAnimation);
-    document.getElementById("stopAnimationBtn").addEventListener(
-        "click", () => cancelAnimationFrame(animationHandle));
-
+    shader.load()
+    window.requestAnimationFrame(animate);
 });
