43f19e7fdf07ed0817440d32907b5b6fc61be156
k.stoetter <katrin.stoetter@stud-mail.uni-wuerzburg.de>
2022-08-16 20:19:32 +0200

    man kann zwischen ray tracer und rasterizer switchen, TODO: Zeige die gleichen Sachen an

--- src/index.ts
+++ src/index.ts
@@ -29,14 +29,11 @@ import phong from "./phong";
 const UNIT_SPHERE = new Sphere(new Vector(0, 0, 0, 1), 1, new Vector(0, 0, 0, 1));
 const UNIT_AABOX = new AABox(new Vector(-0.5, -0.5, -0.5, 1), new Vector(0.5, 0.5, 0.5, 1), new Vector(0, 0, 0, 1));
 
-
-window.addEventListener('load', () => {
-    const canvas = document.getElementById("rasteriser") as HTMLCanvasElement;
-    const gl = canvas.getContext("webgl2");
+window.addEventListener('load', function loadPage() {
 
 
     // construct scene graph
-    const sg = new GroupNode(new Rotation(new Vector(0, 0, 1, 0),0));
+    const sg = new GroupNode(new Rotation(new Vector(0, 0, 1, 0), 0));
     //Texturen
     const textureGeist = new TextureBoxNode('geist.png');
     const textureHCILogo = new TextureBoxNode('hci-logo.png');
@@ -44,18 +41,18 @@ window.addEventListener('load', () => {
     const textureClose = new TextureBoxNode('Icons/close.png');
     //Taskbar
     const TaskBarTr = new GroupNode(new Translation(new Vector(0, -0.95, 0, 0)));
-    const TaskBarSc = new GroupNode(new Scaling(new Vector(3,0.1,0.1,0)))
+    const TaskBarSc = new GroupNode(new Scaling(new Vector(3, 0.1, 0.1, 0)))
     const TaskBarBox = new AABoxNode(new Vector(0, 0, 0, 0));
     TaskBarSc.add(TaskBarBox)
     TaskBarTr.add(TaskBarSc);
     sg.add(TaskBarTr)
     //Icons Transformations
-    const TaskBarIconSc = new GroupNode(new Scaling(new Vector(0.05,0.05,0.05,0.05)));
-    const TaskBarIconTr = new GroupNode(new Translation(new Vector(0.8,0,0.03, 0)));
-    const TaskBarIconTrBox = new GroupNode(new Translation(new Vector(0.6,0,0, 0)));
-    const TaskBarIconScBox = new GroupNode(new Scaling(new Vector(0.05,0.05,0.05,0.05)));
+    const TaskBarIconSc = new GroupNode(new Scaling(new Vector(0.05, 0.05, 0.05, 0.05)));
+    const TaskBarIconTr = new GroupNode(new Translation(new Vector(0.8, 0, 0.03, 0)));
+    const TaskBarIconTrBox = new GroupNode(new Translation(new Vector(0.6, 0, 0, 0)));
+    const TaskBarIconScBox = new GroupNode(new Scaling(new Vector(0.05, 0.05, 0.05, 0.05)));
     //Icon Rosa Kreis
-    const TaskBarIconSphere = new SphereNode(new Vector(1,0.7,0.7,1));
+    const TaskBarIconSphere = new SphereNode(new Vector(1, 0.7, 0.7, 1));
     TaskBarIconSc.add(TaskBarIconSphere);
     TaskBarIconTr.add(TaskBarIconSc);
     TaskBarTr.add(TaskBarIconTr);
@@ -69,8 +66,8 @@ window.addEventListener('load', () => {
     const headerBarTr = new GroupNode(new Translation(new Vector(0.5, 1.9, 0, 0)));
     const headerBarTr2 = new GroupNode(new Translation(new Vector(-0.58, 1.9, 0, 0)));
 
-    const headerBarSc = new GroupNode(new Scaling(new Vector(1,0.1,0.1,0)))
-    const headerBarSc2 = new GroupNode(new Scaling(new Vector(1,0.1,0.1,0)))
+    const headerBarSc = new GroupNode(new Scaling(new Vector(1, 0.1, 0.1, 0)))
+    const headerBarSc2 = new GroupNode(new Scaling(new Vector(1, 0.1, 0.1, 0)))
 
     const headerBarBox = new AABoxNode(new Vector(0, 0, 0, 0));
     headerBarSc.add(headerBarBox)
@@ -81,13 +78,13 @@ window.addEventListener('load', () => {
     headerBarTr2.add(headerBarSc2)
     TaskBarTr.add(headerBarTr2)
     //HeaderBox1 Icon Transformations
-    const headerBarIconBoxSc = new GroupNode(new Scaling(new Vector(0.05,0.05,0.05,0.05)));
-    const headerBarIconBoxTr = new GroupNode(new Translation(new Vector(-0.3,0,0, 0)));
-    const headerBarIconBoxTr2 = new GroupNode(new Translation(new Vector(-0.4,0,0, 0)));
-    const headerBarIconBoxSc2 = new GroupNode(new Scaling(new Vector(0.05,0.05,0.05,0.05)));
+    const headerBarIconBoxSc = new GroupNode(new Scaling(new Vector(0.05, 0.05, 0.05, 0.05)));
+    const headerBarIconBoxTr = new GroupNode(new Translation(new Vector(-0.3, 0, 0, 0)));
+    const headerBarIconBoxTr2 = new GroupNode(new Translation(new Vector(-0.4, 0, 0, 0)));
+    const headerBarIconBoxSc2 = new GroupNode(new Scaling(new Vector(0.05, 0.05, 0.05, 0.05)));
     //HeaderBox2 Icons
-    const headerBarIconBox2Tr = new GroupNode(new Translation(new Vector(-1.3,0,0, 0)));
-    const headerBarIconBox2Tr2 = new GroupNode(new Translation(new Vector(-1.4,0,0, 0)));
+    const headerBarIconBox2Tr = new GroupNode(new Translation(new Vector(-1.3, 0, 0, 0)));
+    const headerBarIconBox2Tr2 = new GroupNode(new Translation(new Vector(-1.4, 0, 0, 0)));
 
     //Header Icons (Vierecke, später Textur drauf)
     const headerBarIconBox = new AABoxNode(new Vector(0, 0, 0, 0));
@@ -108,124 +105,169 @@ window.addEventListener('load', () => {
     headerBarTr.add(headerBarIconBox2Tr2)
 
     //Zeichenflaeche 1
-    const cube = new AABoxNode(new Vector(0,0,0,0));
-    const cubeSc = new GroupNode(new Scaling(new Vector(1,1,1,1)));
+    const cube = new AABoxNode(new Vector(0, 0, 0, 0));
+    const cubeSc = new GroupNode(new Scaling(new Vector(1, 1, 1, 1)));
     const cubeTr = new GroupNode(new Translation(new Vector(-0.5, 0, 0, 0)))
-    const cubeRt = new GroupNode(new Rotation(new Vector(0,1,0,0), 1));
+    const cubeRt = new GroupNode(new Rotation(new Vector(0, 1, 0, 0), 1));
     cubeSc.add(cube);
     cubeSc.add(textureGeist)
     cubeRt.add(cubeSc);
     cubeTr.add(cubeRt)
     sg.add(cubeTr);
     //Zeichenflaeche2
-    const sphere = new SphereNode(new Vector(1,0.7,0.7,1))
-    const sphereSc = new GroupNode(new Scaling(new Vector(0.4,0.4,0.4,1)));
-    const sphereTr = new GroupNode(new Translation(new Vector(0.5,0,0,0)));
+    const sphere = new SphereNode(new Vector(1, 0.7, 0.7, 1))
+    const sphereSc = new GroupNode(new Scaling(new Vector(0.4, 0.4, 0.4, 1)));
+    const sphereTr = new GroupNode(new Translation(new Vector(0.5, 0, 0, 0)));
     sphereSc.add(sphere);
     sphereSc.add(textureHCILogo)
     sphereTr.add(sphereSc);
     sg.add(sphereTr);
 
-
-    // setup for rendering
-    const setupVisitor = new RasterSetupVisitor(gl);
-    setupVisitor.setup(sg);
-
-    const camera = {
-        eye: new Vector(0, 0, -1, 1),
-        center: new Vector(0, 0, 0 ,1),
-        up: new Vector(0, 1, 0, 0),
-        fovy: 60,
-        aspect: canvas.width / canvas.height,
-        near: 0.1,
-        far: 100
-    };
-
-    const phongShader = new Shader(gl,
-        phongVertexShader,
-        phongFragmentShader
-    );
-    const textureShader = new Shader(gl,
-        textureVertexShader,
-        textureFragmentShader
-    );
-    const visitor = new RasterVisitor(gl, phongShader, textureShader, setupVisitor.objects);
-
     let animationNodes = [
-        new RotationNode(cubeRt, new Vector(0,0,1,0)),
+        new RotationNode(cubeRt, new Vector(0, 0, 1, 0)),
     ]
 
-    function simulate(deltaT: number) {
-        for (let animationNode of animationNodes) {
-            animationNode.simulate(deltaT);
-        }
-    }
-
-    let lastTimestamp = performance.now();
-
-    function animate(timestamp: number) {
-        simulate(timestamp - lastTimestamp);
-        visitor.render(sg, camera, []);
-        lastTimestamp = timestamp;
-        window.requestAnimationFrame(animate);
-    }
-
-    Promise.all(
-        [phongShader.load(), textureShader.load()]
-    ).then(x =>
-        window.requestAnimationFrame(animate)
-    );
-
-
-    window.addEventListener('keydown', function (event) {
-        switch (event.key) {
-            case "ArrowUp":
-                animationNodes[0].toggleActive();
-                break;
-        }
-    });
-
-    function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
-        let rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    }
-
-    canvas.addEventListener('mousemove', function(evt) {
-        let mousePos = getMousePos(canvas, evt);
-        let rayFromMouse = new Ray(new Vector(mousePos.x/canvas.width, mousePos.y/canvas.height,0,0), new Vector(0,0,-1,1));
-        let closestIntersection = Infinity;
-        if(UNIT_SPHERE.intersect(rayFromMouse)){
-            console.log("Intersection Detected")
-        }
-    }, false);
-
-    function mouseClickedOn(event: { clientX: number; }){
-        let mx = event.clientX - canvas.getBoundingClientRect().left;
-    }
 
 //Rasterizer und RayTracer Wechseln
-    const btn1 = document.getElementById('btnradio1');
-    const btn2 = document.getElementById('btnradio2');
+    const canvas = document.getElementById("rasteriser") as HTMLCanvasElement;
+    const canvas2 = document.getElementById("rayTracer") as HTMLCanvasElement;
 
-    btn2.addEventListener("click", function() {
+    const btn1 = document.getElementById('btnradio1') as HTMLInputElement;
+    const btn2 = document.getElementById('btnradio2') as HTMLInputElement;
+
+    let renderer = sessionStorage.getItem("renderer")
+    console.log(renderer)
+    if (renderer=="rasterizer"){
+        btn1.checked = true
+    } else {
+        btn2.checked = true
+    }
+    console.log(btn1.checked)
+    console.log(btn2.checked)
+    if (btn1.checked) {
+        // canvas2.style.visibility='hidden';
+        // canvas.style.visibility='visible';
+        rasterVisitor()
+    } else if (btn2.checked) {
+        // canvas.style.visibility='hidden';
+        // canvas2.style.visibility='visible';
+        btn2.checked = true
+        rayVisitor()
+    }
+
+    function rasterVisitor() {
+        canvas2.style.display = "none"
+        canvas.style.display = "block"
+        const gl = canvas.getContext("webgl2");
+
+
+        // setup for rendering
+        const setupVisitor = new RasterSetupVisitor(gl);
+        setupVisitor.setup(sg);
+
+        const camera = {
+            eye: new Vector(0, 0, -1, 1),
+            center: new Vector(0, 0, 0, 1),
+            up: new Vector(0, 1, 0, 0),
+            fovy: 60,
+            aspect: canvas.width / canvas.height,
+            near: 0.1,
+            far: 100
+        };
+
+        const phongShader = new Shader(gl,
+            phongVertexShader,
+            phongFragmentShader
+        );
+        const textureShader = new Shader(gl,
+            textureVertexShader,
+            textureFragmentShader
+        );
+        const visitor = new RasterVisitor(gl, phongShader, textureShader, setupVisitor.objects);
+
+        function simulate(deltaT: number) {
+            for (let animationNode of animationNodes) {
+                animationNode.simulate(deltaT);
+            }
+        }
+
+        let lastTimestamp = performance.now();
+
+        function animate(timestamp: number) {
+            simulate(timestamp - lastTimestamp);
+            visitor.render(sg, camera, []);
+            lastTimestamp = timestamp;
+            window.requestAnimationFrame(animate);
+        }
+
+        Promise.all(
+            [phongShader.load(), textureShader.load()]
+        ).then(x =>
+            window.requestAnimationFrame(animate)
+        );
+
+
+        window.addEventListener('keydown', function (event) {
+            switch (event.key) {
+                case "ArrowUp":
+                    animationNodes[0].toggleActive();
+                    break;
+            }
+        });
+
+        function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
+            let rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+        }
+
+        canvas.addEventListener('mousemove', function (evt) {
+            let mousePos = getMousePos(canvas, evt);
+            let rayFromMouse = new Ray(new Vector(mousePos.x / canvas.width, mousePos.y / canvas.height, 0, 0), new Vector(0, 0, -1, 1));
+            let closestIntersection = Infinity;
+            if (UNIT_SPHERE.intersect(rayFromMouse)) {
+                console.log("Intersection Detected")
+            }
+        }, false);
+
+        function mouseClickedOn(event: { clientX: number; }) {
+            let mx = event.clientX - canvas.getBoundingClientRect().left;
+        }
+
+    }
+
+    function rayVisitor() {
+        canvas.style.display = "none"
+        canvas2.style.display = "block"
+        // canvas.hidden
         console.log("RayTracer")
-
-        const canvas2 = document.getElementById("rayTracer") as HTMLCanvasElement;
         const ctx = canvas2.getContext("2d");
-        const lightPositions = [
-            new Vector(1, 1, -1, 1)
-        ];
+
         const gnRotation = new Rotation(new Vector(1, 0, 0, 0), 0)
         const gn = new GroupNode(gnRotation);
         sg.add(gn);
 
+        const gn1 = new GroupNode(new Translation(new Vector(0.5, .5, 0, 0)));
+        gn.add(gn1);
+        gn1.add(new SphereNode(new Vector(.4, 0, 0, 1)));
+
+        const gn2 = new GroupNode(new Translation(new Vector(-0.5, 1, 1, 0)));
+        gn.add(gn2);
+
+        const gn3 = new GroupNode(new Scaling(new Vector(0.4, 0.4, 0.4, 0)));
+        gn2.add(gn3);
+
+        gn3.add(new SphereNode(new Vector(0, 0, .3, 1)));
+
+        const lightPositions = [
+            new Vector(1, 1, 1, 1)
+        ];
         const camera = {
             origin: new Vector(0, 0, 0, 1),
-            width: canvas2.width,
-            height: canvas2.height,
+            width: canvas.width,
+            height: canvas.height,
             alpha: Math.PI / 3
         }
 
@@ -236,6 +278,7 @@ window.addEventListener('load', () => {
         let lastTimestamp = 0;
         let animationTime = 0;
         let animationHasStarted = true;
+
         function animate(timestamp: number) {
             let deltaT = timestamp - lastTimestamp;
             if (animationHasStarted) {
@@ -244,7 +287,7 @@ window.addEventListener('load', () => {
             }
             animationTime += deltaT;
             lastTimestamp = timestamp;
-            gnRotation.angle = animationTime / 2000;
+            animationNodes[0].angle = animationTime / 2000;
 
             visitor.render(sg, camera, lightPositions);
             // animationHandle = window.requestAnimationFrame(animate);
@@ -255,26 +298,36 @@ window.addEventListener('load', () => {
                 window.cancelAnimationFrame(animationHandle);
             }
             animationHasStarted = true;
+
             function animation(t: number) {
                 animate(t);
                 animationHandle = window.requestAnimationFrame(animation);
             }
+
             animationHandle = window.requestAnimationFrame(animation);
         }
+
         animate(0);
 
         document.getElementById("startAnimationBtn").addEventListener(
             "click", startAnimation);
         document.getElementById("stopAnimationBtn").addEventListener(
             "click", () => cancelAnimationFrame(animationHandle));
+    }
 
-
-
+    window.addEventListener('click', function (){
+        if (btn1.checked) {
+            console.log("render")
+            sessionStorage.setItem("renderer", "rasterizer")
+            // rasterVisitor()
+        } else if (btn2.checked) {
+            console.log("ray")
+            sessionStorage.setItem("renderer", "rayTracer")
+            // rayVisitor()
+        }
+        location.reload()
     });
 
-    btn1.addEventListener('click', function (){
-        location.reload()
-    })
 });
 
 
